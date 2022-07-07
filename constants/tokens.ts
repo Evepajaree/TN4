db2019f2550e41afed01982336559cf537b27f2d
Evepajaree <eve_102@hotmail.co.th>
2022-07-07 11:15:46 +0700

    main

---
+++ constants/tokens.ts
@@ -0,0 +1,31 @@
+export const ETH_TOKENS = [
+    {
+        name: "USD Theter",
+        symbol: "USDT",
+        decimals: 6,
+        imageUrl: "https://cryptologos.cc/logos/tether-usdt-logo.png?v=022",
+        address: "0x07de306FF27a2B630B1141956844eB1552B956B5"
+    }
+]
+
+export const RINKEBY_TOKENS = [
+    {
+        name: "USD Theter",
+        symbol: "USDT",
+        decimals: 6,
+        imageUrl: "https://cryptologos.cc/logos/tether-usdt-logo.png?v=022",
+        address: "0x07de306FF27a2B630B1141956844eB1552B956B5"
+    }
+
+]
+
+export const KOVAN_TOKENS = [
+    {
+        name: "USD Theter",
+        symbol: "USDT",
+        decimals: 6,
+        imageUrl: "https://cryptologos.cc/logos/tether-usdt-logo.png?v=022",
+        address: "0x07de306FF27a2B630B1141956844eB1552B956B5"
+    }
+
+]
